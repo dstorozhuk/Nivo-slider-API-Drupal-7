0497e9dfea9ef1a3c661be3c24dc544ec9e3f312
Dmitriy Storozhuk <dmutro.storozhuk@gmail.com>
2014-05-26 15:42:56 +0300

    Convert template to twig, use own js name space, register js library.

--- nivo_slider_api.js
+++ nivo_slider_api.js
@@ -1,36 +1,40 @@
-(function($) {
+(function ($, w) {
 
-    Drupal.behaviors.nivo_slider_api = {
-        attach: function(context) {
-            $('.nivo-slider-api', context).once('nivo-slider-api', function() {
-                var vns = $(this);
-                var id = vns.attr('id');
-                var cfg = Drupal.settings.nivo_slider_api[id];
-                var final_cfg = cfg;
-                // add theme callback
-                if (typeof Drupal.settings.nivo_slider_api[id].theme_name !== 'undefined') {
-                    var theme_name = Drupal.settings.nivo_slider_api[id].theme_name;
-                    var theme_callbacks = Drupal.nivo_slider_api.themes[theme_name].callbacks;
-                    Drupal.nivo_slider_api.themes[theme_name].box = vns;
-                    final_cfg = $.extend({}, cfg, theme_callbacks);
-                }
+  "use strict";
 
-                // Fix sizes
-                vns.data('hmax', 0).data('wmax', 0);
-                $('img', vns).each(function() {
-                    $(this).load(function() {
-                        hmax = (vns.data('hmax') > $(this).height()) ? vns.data('hmax') : $(this).height();
-                        wmax = (vns.data('wmax') > $(this).width()) ? vns.data('wmax') : $(this).width();
-                        vns.width(wmax).height(hmax).data('hmax', hmax).data('wmax', wmax);
-                    });
-                });
+  w.NivoSliderApi = w.NivoSliderApi || {themes: {}};
 
-                vns.nivoSlider(final_cfg);
-            });
+  Drupal.behaviors.nivo_slider_api = {
+    attach: function (context) {
+      $('.nivo-slider-api', context).once('nivo-slider-api', function () {
+        var vns = $(this);
+        var id = vns.attr('id');
+        var cfg = drupalSettings.nivo_slider_api[id];
+        var final_cfg = cfg;
+        // add theme callback
+        if (typeof drupalSettings.nivo_slider_api[id].theme_name !== 'undefined') {
+          var theme_name = drupalSettings.nivo_slider_api[id].theme_name;
+          var theme_callbacks = NivoSliderApi.themes[theme_name].callbacks;
+          NivoSliderApi.themes[theme_name].box = vns;
+          final_cfg = $.extend({}, cfg, theme_callbacks);
         }
-    };
-    // name space for themes
-    Drupal.nivo_slider_api = Drupal.nivo_slider_api || {
-        themes: {}
-    };
-})(jQuery);
+
+        // Fix sizes
+        vns.data('hmax', 0).data('wmax', 0);
+        $('img', vns).each(function () {
+          $(this).load(function () {
+            var hmax = (vns.data('hmax') > $(this).height()) ? vns.data('hmax') : $(this).height();
+            var wmax = (vns.data('wmax') > $(this).width()) ? vns.data('wmax') : $(this).width();
+            vns.width(wmax).height(hmax).data('hmax', hmax).data('wmax', wmax);
+          });
+        });
+
+        vns.nivoSlider(final_cfg);
+      });
+    }
+  };
+  // name space for themes
+//  NivoSliderApi = NivoSliderApi || {
+//    themes: {}
+//  };
+})(jQuery, window);
--- themes/simplejstheme/simplejstheme.js
+++ themes/simplejstheme/simplejstheme.js
@@ -1,18 +1,18 @@
 (function ($) {
 
-Drupal.nivo_slider_api.themes.simplejstheme = Drupal.nivo_slider_api.themes.simplejstheme || {callbacks: {}};
+NivoSliderApi.themes.simplejstheme = NivoSliderApi.themes.simplejstheme || {callbacks: {}};
 
-Drupal.nivo_slider_api.themes.simplejstheme.callbacks  = {
+NivoSliderApi.themes.simplejstheme.callbacks  = {
     afterChange: function(){
-        var box = Drupal.nivo_slider_api.themes.simplejstheme.box;
+        var box = NivoSliderApi.themes.simplejstheme.box;
         var current_slide_no = box.data('nivo:vars').currentSlide;
-        Drupal.nivo_slider_api.themes.simplejstheme.box_status_current.html(current_slide_no + 1);
+        NivoSliderApi.themes.simplejstheme.box_status_current.html(current_slide_no + 1);
     },
     afterLoad: function(){
 
-      var box = Drupal.nivo_slider_api.themes.simplejstheme.box;
+      var box = NivoSliderApi.themes.simplejstheme.box;
       var box_status = $('<div id="nivo-slider-status"></div>');
-      var box_status_current = Drupal.nivo_slider_api.themes.simplejstheme.box_status_current = $('<span class="current-slide"></span>');
+      var box_status_current = NivoSliderApi.themes.simplejstheme.box_status_current = $('<span class="current-slide"></span>');
       var box_status_total = $('<span class="total-slides"></span>');
       var total = box.data('nivo:vars').totalSlides;
       var current_slide_no = box.data('nivo:vars').currentSlide;
